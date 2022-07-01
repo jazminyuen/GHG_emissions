c7a5ffee2222653e96cf5baad96a4bfbc9357c40
Vanessa <vanessacox6@gmail.com>
2022-06-30 18:41:40 -0700

    edited geojson file

--- basins_map/static/js/logic.js
+++ basins_map/static/js/logic.js
@@ -3,25 +3,43 @@ console.log("working");
 
 // Create the map object with a center and zoom level.
 let map = L.map("mapid", {
-    center: [
-      40.7, -94.5
-    ],
-    zoom: 4
-  });
-
-  // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    id: 'mapbox/streets-v11',
-    tileSize: 512,
-    zoomOffset: -1,
-    accessToken: API_KEY
+  center: [
+    40.7, -94.5
+  ],
+  zoom: 4
 });
-// Then we add our 'graymap' tile layer to the map.
+
+// We create the tile layer that will be the background of our map.
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+  attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+  maxZoom: 18,
+  id: 'mapbox/streets-v11',
+  tileSize: 512,
+  zoomOffset: -1,
+  accessToken: API_KEY
+});
+// Add our 'graymap' tile layer to the map.
 streets.addTo(map);
 
-// Accessing the airport GeoJSON data
-let basinData = "./basins.json";
 
-console.log(basinData);
+// Accessing the basin GeoJSON data
+let basinData = "https://raw.githubusercontent.com/jazminyuen/GHG_emissions/vanessa/basins_map/static/basins.json";
+
+
+
+// Grabbing our GeoJSON data.
+d3.json(basinData).then(function (x) {
+  console.log(x);
+  // Creating a GeoJSON layer with the retrieved data.
+  L.geoJSON(x).addTo(map);
+  
+});
+
+
+// d3.json(basinData).then(function(data) {
+//   for (var i = 0; i < data.length; i++) {
+//     console.log(data[i].geometry.coordinates);
+//     L.marker(data[i].geometry.coordinates).addTo(map);
+//       //console.log(data[i].properties);
+//   }
+// });
