edc5acf2d65c831e1ad460803ff60aa547729cf9
Jazmin Capuli Yuen <jaz@jazmins-air.lan>
2022-07-11 11:08:28 -0700

    Updated summary and sector charts for dashboard

--- jazmin/app.py
+++ jazmin/app.py
@@ -11,14 +11,14 @@ from sqlalchemy import text
 app = Dash(__name__)
 
 # Setup layout
-# app.layout = html.Div([
-#     html.H4('Emissions by Top Sectors'),
-#     dcc.Graph(id="graph"),
-#     dcc.Dropdown(id="dropdown", options=['Power Plants', 'Waste', 'Other', 'Petroleum and Natural Gas Systems',
-#        'Minerals', 'Chemicals', 'Metals', 'Pulp and Paper', 'Other,Waste','Pulp and Paper,Waste',
-#        'Natural Gas and Natural Gas Liquids Suppliers,Petroleum and Natural Gas Systems',
-#        'Petroleum Product Suppliers,Refineries'])
-# ])
+app.layout = html.Div([
+    html.H4('Emissions by Top Sectors'),
+    dcc.Graph(id="graph"),
+    dcc.Dropdown(id="dropdown", options=['Power Plants', 'Waste', 'Other', 'Petroleum and Natural Gas Systems',
+       'Minerals', 'Chemicals', 'Metals', 'Pulp and Paper', 'Other,Waste','Pulp and Paper,Waste',
+       'Natural Gas and Natural Gas Liquids Suppliers,Petroleum and Natural Gas Systems',
+       'Petroleum Product Suppliers,Refineries'])
+])
 
 
 
@@ -34,38 +34,36 @@ query_summary = text('SELECT * FROM summary')
 summary_df = pd.read_sql(query_summary, engine)
 
 # Layout
-app.layout = html.Div([
-    html.H4('Total Emissions Stats'),
-    dcc.Graph(id="graph"),
-    dcc.Checklist(id="checklist", options=["mean","median","sum"])
-])
+# app.layout = html.Div([
+#     html.H4('Total Emissions'),
+#     dcc.Graph(id="graph"),
+#     dcc.Checklist(id="checklist", options=["mean","median","sum"], inline=True)
+# ])
 
 # Create callback for emissions by sector
-# @app.callback(
-#     Output("graph", "figure"), 
-#     Input("dropdown", "value"))
-# def update_sector_chart(sector):
-#     df = em_df.loc[em_df["Sector"]==sector]
-#     df.drop(["Sector"], axis=1, inplace=True)
-#     sum = df.sum(axis=0).tolist()
-#     year = ['2011', '2012', '2013', '2014', '2015', '2016', '2017', '2018', '2019',
-#        '2020'] 
-#     # mean = df.mean(axis=0).tolist()
-#     fig = px.bar(x=year,y=sum,title=f'Emissions by Sector: {sector}',labels={'y':'Emissions', 'x':'Year'})
-#     fig.update_traces(marker_color='rgb(158,202,225)', marker_line_color='rgb(8,48,107)',
-#                   marker_line_width=1.5, opacity=0.6)
-#     return fig
-
-# Create callback for summary charts, average/median/sum overall for each year
 @app.callback(
     Output("graph", "figure"), 
-    Input("checklist", "value"))
-def update_summary_chart(value):
-    df = summary_df
-    fig = px.line(df, x="year", y=value, labels={"year":"Year", "value":f"{value}"})
-    fig.update_traces(marker_color='rgb(158,202,225)', marker_line_color='rgb(8,48,107)',
-                  marker_line_width=1.5, opacity=0.6)
+    Input("dropdown", "value"))
+def update_sector_chart(sector):
+    df = em_df.loc[em_df["Sector"]==sector]
+    df.drop(["Sector"], axis=1, inplace=True)
+    sum = df.sum(axis=0).tolist()
+    year = ['2011', '2012', '2013', '2014', '2015', '2016', '2017', '2018', '2019',
+       '2020'] 
+    fig = px.bar(x=year,y=sum,title=f'Emissions by Sector: {sector}',labels={'y':'Emissions', 'x':'Year'})
+    fig.update_traces(marker_color='rgb(158,202,225)',marker_line_color='rgb(8,48,107)',marker_line_width=2.5, opacity=0.6)
     return fig
+
+# Create callback for summary charts, average/median/sum overall for each year
+# @app.callback(
+#     Output("graph", "figure"), 
+#     Input("checklist", "value"))
+# def update_summary_chart(value):
+#     df = summary_df
+#     year = ['2011', '2012', '2013', '2014', '2015', '2016', '2017', '2018', '2019','2020'] 
+#     fig = px.line(df, x=year, y=value, markers=True, labels=dict(value="Emissions", x="Year", variable="Metric"), title="Total Emissions")
+#     fig.update_traces(marker_color='rgb(158,202,225)', marker_line_color='rgb(8,48,107)', marker_line_width=3.0, opacity=0.6)
+#     return fig
     
 
 app.run_server(debug=True)
