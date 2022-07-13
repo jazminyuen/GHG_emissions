75e56c13d6114eeca24bcd56ee3f899b830efee4
Carly Magiera <carlymagiera@Carlys-MacBook-Pro.local>
2022-07-12 17:08:20 -0700

    update graph colors in app

--- carly/dashapp3.py
+++ carly/dashapp3.py
@@ -1,35 +1,35 @@
-#import dependencies
-
-from itertools import dropwhile
+# import dependencies
 import dash
 import dash_bootstrap_components as dbc
-import dash_core_components as dcc
-import dash_html_components as html
+from dash import dcc
+from dash import html
 from dash.dependencies import Input, Output, State
 import pandas as pd
 import sqlalchemy as sa
 from sqlalchemy import create_engine
+from sqlalchemy import text
 import plotly.graph_objects as go
-import json
 import plotly.express as px
 from sklearn.preprocessing import StandardScaler
 from sklearn.cluster import KMeans
-from sqlalchemy import text
 
 
-
-
-
-#connect to database
+# connect to database and query data
 db_string = 'postgresql://ghg_emissions:Password00@ghgemissions.cg3dqiowwhnr.us-west-1.rds.amazonaws.com:5432/ghg_data'
 engine = create_engine(db_string)
 
+# query for sector emissions bar chart
+query = text('SELECT * FROM sector_emissions')
+sector_df = pd.read_sql(query,engine)
+sector_df = sector_df.rename(columns={"_2011":"2011","_2012":"2012","_2013":"2013","_2014":"2014","_2015":"2015","_2016":"2016","_2017":"2017","_2018":"2018","_2019":"2019","_2020":"2020","sector":"Sector"})
+
+# query for total emissions summary line chart
+query_summary = text('SELECT * FROM summary')
+summary_df = pd.read_sql(query_summary, engine)
+
+# query for total emissions by year bar chart
 query = text('SELECT * FROM direct_emissions')
-
 df = pd.read_sql(query,engine)
-
-
-# creating the datasets
 total_2011 = df["total_emissions_2011"].sum()
 total_2012 = df["total_emissions_2012"].sum()
 total_2013 = df["total_emissions_2013"].sum()
@@ -41,47 +41,18 @@ total_2018 = df["total_emissions_2018"].sum()
 total_2019 = df["total_emissions_2019"].sum()
 total_2020 = df["total_emissions_2020"].sum()
 
+# create the datasets for total emissions by year bar chart
 data = {'2011':total_2011, '2012':total_2012, '2013':total_2013, '2014':total_2014,'2015':total_2015,'2016':total_2016,'2017':total_2017,'2018':total_2018,'2019':total_2019, '2020':total_2020,  }
 years = list(data.keys())
 emissions = list(data.values())
 table = {'Year':years, 'emissions':emissions}
 emissions_df = pd.DataFrame(table)
 
+# figure - total emissions by year bar chart
+fig1 = px.bar(emissions_df, x='Year', y='emissions', title="Total Direct Emissions by Year", labels={"emissions":"Emissions"}, color="emissions", color_continuous_scale='ylorrd')
+fig1.update_traces( marker_line_width=3.0, opacity=0.8)
 
-#jazmin summary data
-
-em_df = pd.DataFrame(df[["total_emissions_2011",
-                                "total_emissions_2012",
-                                "total_emissions_2013",
-                                "total_emissions_2014",
-                                "total_emissions_2015",
-                                "total_emissions_2016",
-                                "total_emissions_2017",
-                                "total_emissions_2018",
-                                "total_emissions_2019",
-                                "total_emissions_2020"]])
-# Rename columns
-em_df = em_df.rename(columns={"total_emissions_2011":"2011",
-                                "total_emissions_2012":"2012",
-                                "total_emissions_2013":"2013",
-                                "total_emissions_2014":"2014",
-                                "total_emissions_2015":"2015",
-                                "total_emissions_2016":"2016",
-                                "total_emissions_2017":"2017",
-                                "total_emissions_2018":"2018",
-                                "total_emissions_2019":"2019",
-                                "total_emissions_2020":"2020"})
-# Create summary df
-summary = em_df.describe()
-summary_df = pd.DataFrame(summary)
-summary_df = summary_df.transpose()
-summary_df["sum"] = em_df.sum().to_list()
-
-
-
-
-#vanessa ml figure
-
+# create the datasets for ml figure
 direct_emitters_df = df[['industry_type_sector', 'total_emissions_2020', 'total_emissions_2019', 'total_emissions_2018',
        'total_emissions_2017', 'total_emissions_2016', 'total_emissions_2015',
        'total_emissions_2014', 'total_emissions_2013', 'total_emissions_2012',
@@ -141,12 +112,28 @@ direct_emitters_total["industry_type_sector"] = direct_emitters_total["industry_
 direct_emitters_total['class'] = direct_emitters_total['class'].replace(['0','1','2'],['Low','Mid','High'])
 direct_emitters_total["class"] = direct_emitters_total["class"].astype(str)
 
+# figure - total emissions by sector by class scatter plot
+mlfig = px.scatter(data_frame=direct_emitters_total, x="total_emissions", y="industry_type_sector", color="class",
+           size="total_emissions", color_discrete_sequence=["gold", "darkorange", "red"],
+                           labels={
+                     "total_emissions": "Total Emissions (metric tons CO2 equivalent)",
+                     "industry_type_sector": "Industry Type (Sector)"
+                 },
+                title="Direct Emitters Classification Model", size_max=35, opacity=0.7)
 
+# figure - total emissions by sector by class 3D scatter plot
+fig3d = px.scatter_3d(direct_emitters_total, x="total_emissions", y="industry_type_sector", z="class", color="class", title="Direct Emitters Classification Model 3D",
+           color_discrete_sequence=["yellow", "orange", "red"],
+           labels={
+                     "total_emissions": "Total Emissions (metric tons CO2 equivalent)",
+                     "industry_type_sector": "Industry Type (Sector)"},
+                      width=800)
+fig3d.update_layout(legend=dict(x=0,y=1))
 
 
 # app design
 
-# the style arguments for the sidebar.
+# the style arguments for the sidebar
 SIDEBAR_STYLE = {
     'position': 'fixed',
     'top': 0,
@@ -162,10 +149,12 @@ SIDE_TEXT_STYLE = {
     'color': '#FFFFFF'
 }
 
-# the style arguments for the main content page.
+# the style arguments for the main content page
 CONTENT_STYLE = {
     'margin-left': '25%',
     'margin-right': '5%',
+    'margin-top': '2%',
+    'margin-bottom': '5%',
     'padding': '20px 10p'
 }
 
@@ -174,23 +163,18 @@ TEXT_STYLE = {
     'color': '#608B74'
 }
 
-
-
 CARD_TEXT_STYLE = {
     'textAlign': 'center',
     'color': '#608B74'
 }
 
-fig3d = px.scatter_3d(direct_emitters_total, x="total_emissions", y="industry_type_sector", z="class", color="class",
-           color_discrete_sequence=["yellow", "orange", "red"], width=800)
-fig3d.update_layout(legend=dict(x=0,y=1))
 
 
 
-
+# sidebar layout
 sidebar = html.Div(
     [
-        html.H2('About the Data', style=SIDE_TEXT_STYLE),
+        html.H3('About the Data', style=SIDE_TEXT_STYLE),
         html.Hr(),
         dbc.Card(
             [
@@ -205,6 +189,7 @@ sidebar = html.Div(
     style=SIDEBAR_STYLE,
 )
 
+# first row layout
 content_first_row = dbc.Row([
     dbc.Col(
         dbc.Card(
@@ -212,7 +197,7 @@ content_first_row = dbc.Row([
 
                 dbc.CardBody(
                     [
-                        html.H4(id='card_title_1', children=['greenhouse gases trap heat in our atmosphere'], className='card-title',
+                        html.H4(id='card_title_1', children=['Greenhouse gases trap heat in our atmosphere'], className='card-title',
                                 style=CARD_TEXT_STYLE),
                         html.P(id='card_text_1', children=['The most abundant greenhouse gases are Carbon Dioxide, Methane, Nitrous Oxide, and Fluorinated Gases'], style=CARD_TEXT_STYLE),
                     ]
@@ -227,8 +212,8 @@ content_first_row = dbc.Row([
 
                 dbc.CardBody(
                     [
-                        html.H4(id='card_title_2', children=['The largest sources of emissions are from Transportation, Industry, and Electricity'],className='card-title', style=CARD_TEXT_STYLE),
-                        html.P(['Almost all of the increase of greenhouse gases in the atmosphere over the last 150 years is due to human activity'], style=CARD_TEXT_STYLE),
+                        html.H4(id='card_title_3', children=['In 2020, U.S. CO2 emissions totaled 5,222 million metric tons of carbon dioxide equivalents'],className='card-title', style=CARD_TEXT_STYLE),
+                        html.P(['CO2 emissions have decreased by 10.3 percent from 2019 to 2020'], style=CARD_TEXT_STYLE),
                     ]
                 ),
             ]
@@ -237,29 +222,17 @@ content_first_row = dbc.Row([
         md=3
     ),
     dbc.Col(
-            dcc.Graph(figure = fig3d), md=6
+            dcc.Graph(figure = fig1), md=6
         ),
 
 ])
-fig1 = px.bar(emissions_df, x='Year', y='emissions', title="Total Direct Emissions by Year", color="emissions", color_continuous_scale= 'ylorrd' )
-mlfig = px.scatter(data_frame=direct_emitters_total, x="total_emissions", y="industry_type_sector", color="class",
-           size="total_emissions", color_discrete_sequence=["gold", "darkorange", "red"],
-                           labels={
-                     "total_emissions": "Total Emissions (metric tons CO2 equivalent)",
-                     "industry_type_sector": "Industry Type (Sector)",
-                 },
-                title="Direct Emitters Classification Model", size_max=35, opacity=0.7)
 
-
-mapfig = px.scatter_geo(basins, locations="type")
-
-
-
+# second row layout
 content_second_row = dbc.Row(
     [
         dbc.Col(
             
-            dcc.Graph(figure=fig1), md=6,
+            dcc.Graph(figure=fig3d), md=6,
             
         ),
         dbc.Col(
@@ -268,8 +241,8 @@ content_second_row = dbc.Row(
 
                 dbc.CardBody(
                     [
-                        html.H4(id='card_title_3', children=['In 2020, U.S. CO2 emissions totaled 5,222 million metric tons of carbon dioxide equivalents'],className='card-title', style=CARD_TEXT_STYLE),
-                        html.P(['CO2 emissions have decreased by 10.3 percent from 2019 to 2020'], style=CARD_TEXT_STYLE),
+                        html.H4(id='card_title_2', children=['The largest sources of emissions are from Transportation, Industry, and Electricity'],className='card-title', style=CARD_TEXT_STYLE),
+                        html.P(['Almost all of the increase of greenhouse gases in the atmosphere over the last 150 years is due to human activity'], style=CARD_TEXT_STYLE),
                     ]
                 ),
             ]
@@ -279,9 +252,7 @@ content_second_row = dbc.Row(
     ),
     ])
 
-  
-
-secfig = px.bar(direct_emitters_total, x="industry_type_sector", y='total_emissions')
+# third row layout
 content_third_row = dbc.Row(
     [
         dbc.Col(
@@ -290,50 +261,39 @@ content_third_row = dbc.Row(
     ]
 )
 
-
-
-
-
+# fourth row layout
 content_fourth_row = dbc.Row(
      [
         dbc.Col(
         html.Div([
-        html.H4('Total Emissions'),
+        html.H4(),
         dcc.Graph(id="emissionsgraph"),
-        dcc.Checklist(id="checklist", options=["mean","median","sum"], inline=True)
+        dcc.Checklist(id="checklist", options=["mean","median","sum"])
 ])
-        ),
-        # dbc.Col(
-        #     dcc.Graph(id='graph_1', figure = secfig), md=4
-        # )
+        )
     ]
 )
 
+# fifth row layout
 content_fifth_row = dbc.Row(
      [
         dbc.Col(
         html.Div([
-        html.H4('Emissions by Top Sectors'),
+        html.H4(),
         dcc.Graph(id="sectorgraph"),
         dcc.Dropdown(id="dropdown2", options=['Power Plants', 'Waste', 'Other', 'Petroleum and Natural Gas Systems',
-       'Minerals', 'Chemicals', 'Metals', 'Pulp and Paper', 'Other,Waste','Pulp and Paper,Waste',
-       'Natural Gas and Natural Gas Liquids Suppliers,Petroleum and Natural Gas Systems',
-       'Petroleum Product Suppliers,Refineries'])
+        'Minerals', 'Chemicals', 'Metals', 'Pulp and Paper', 'Other,Waste','Pulp and Paper,Waste',
+        'Natural Gas and Natural Gas Liquids Suppliers,Petroleum and Natural Gas Systems',
+        'Petroleum Product Suppliers,Refineries'])
 ])
-        ),
-        # dbc.Col(
-        #     dcc.Graph(id='graph_1', figure = secfig), md=4
-        # )
+        )
     ]
 )
 
-
-
-
-
+# content holds set layout and style variables
 content = html.Div(
     [
-        html.H2('Greenhouse Gas Emissions Dashboard', style=TEXT_STYLE),
+        html.H1('Greenhouse Gas Emissions Dashboard', style=TEXT_STYLE),
         html.Hr(),
         content_first_row,
         content_third_row,
@@ -344,31 +304,37 @@ content = html.Div(
     style=CONTENT_STYLE
 )
 
+# initialize app with set layouts
 app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
 app.layout = html.Div([sidebar, content])
 
 
-
-# @app.callback(
-#     Output("graph", "figure"), 
-#     Input("dropdown", "value"))
-# def update_bar_chart(day):
-#     df = df # replace with your own data source
-#     figselect = px.bar(df, x="industry_type_sector", y="total_emissions_2020", 
-#                  color='rgb(158,202,225)', barmode="group")
-#     return fig
-
+# create callback to update total emissions summary line chart
 @app.callback(
-    Output("chart", "figure"), 
+    Output("emissionsgraph", "figure"), 
+    Input("checklist", "value"))
+def update_summary_chart(value):
+    df = summary_df
+    year = ['2011', '2012', '2013', '2014', '2015', '2016', '2017', '2018', '2019','2020'] 
+    fig = px.line(df, x=year, y=value, markers=True, color_discrete_sequence=["gold", "darkorange", "red"], labels=dict(value="Total Emissions (metric tons CO2 equivalent)", x="Year", variable="Metric"), title="Total Emissions Summary")
+    fig.update_traces( marker_line_width=3.0, opacity=0.8)
+    return fig
+
+# create callback to update sector emissions bar chart
+@app.callback(
+    Output("sectorgraph", "figure"), 
     Input("dropdown2", "value"))
-def update_line_chart(continents):
-    df = summary_df # replace with your own data source
-    #mask = df.continent.isin(continents)
-    fig = px.line(df,y="mean")
+def update_sector_chart(sector):
+    df = sector_df.loc[sector_df["Sector"]==sector]
+    df.drop(["Sector"], axis=1, inplace=True)
+    sum = df.sum(axis=0).tolist()
+    year = ['2011', '2012', '2013', '2014', '2015', '2016', '2017', '2018', '2019',
+       '2020'] 
+    fig = px.bar(x=year,y=sum, color= sum, color_continuous_scale='ylorrd', title=f'Emissions by Sector: {sector}',labels={'y':'Total Emissions (metric tons CO2 equivalent)', 'x':'Year'})
+    fig.update_traces(marker_line_width=2.5, opacity=0.8)
     return fig
 
 
-
-
+# run app
 if __name__ == '__main__':
     app.run_server(port='8085')
